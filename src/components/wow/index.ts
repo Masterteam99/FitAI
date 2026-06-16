670c71eaaa8873f240220a44ce1e63e6891a1551
masterteam99 <masterteam99.ab@gmail.com>
2026-06-17 00:48:17 +0200

    feat(wow): export ExerciseFormPlayer e ScrollExplainer

--- src/components/wow/index.ts
+++ src/components/wow/index.ts
@@ -3,4 +3,7 @@ export { AnimatedRing } from "./charts/AnimatedRing";
 export { RadialGauge } from "./charts/RadialGauge";
 export { AnimatedArea } from "./charts/AnimatedArea";
 export { AnimatedBars } from "./charts/AnimatedBars";
+export { ExerciseFormPlayer } from "./ExerciseFormPlayer";
+export { ScrollExplainer } from "./ScrollExplainer";
 export * from "./heat/heatScale";
+export * from "./pose/poseEngine";
