b337f899a7751c873a24f91975ab0c42b2c6c7fb
masterteam99 <masterteam99.ab@gmail.com>
2026-06-17 00:29:03 +0200

    feat(wow): barrel export libreria wow

---
+++ src/components/wow/index.ts
@@ -0,0 +1,6 @@
+export { AdaptiveBodyMap } from "./AdaptiveBodyMap";
+export { AnimatedRing } from "./charts/AnimatedRing";
+export { RadialGauge } from "./charts/RadialGauge";
+export { AnimatedArea } from "./charts/AnimatedArea";
+export { AnimatedBars } from "./charts/AnimatedBars";
+export * from "./heat/heatScale";
